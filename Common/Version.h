5026ad5410d69af995fe439abaa17666906b3b1c
Sir Graham <sirgraham@endorasoft.com>
2023-03-18 20:55:25 +0100

    Change subversion.

--- Common/Version.h
+++ Common/Version.h
@@ -38,7 +38,7 @@
 #define VERSION_NAME         "GEN FrameWork"
 #define VERSION_VERSION      0
 #define VERSION_SUBVERSION   13
-#define VERSION_SUBERROR     2
+#define VERSION_SUBERROR     3
 #define VERSION_CODENAME     "Ozones B3"
 
 /*---- CLASS ---------------------------------------------------------------------------------------------------------*/
