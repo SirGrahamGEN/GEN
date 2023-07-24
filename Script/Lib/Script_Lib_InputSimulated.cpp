b219df5db229f6d9741181287d1445644cefbccc
Sir Graham <sirgraham@endorasoft.com>
2023-07-24 14:07:03 +0200

    Add Scrip Lib for Input Simulated

---
+++ Script/Lib/Script_Lib_InputSimulated.cpp
@@ -0,0 +1,194 @@
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @file       Script_Lib_InputSimulated.cpp
+* 
+* @class      SCRIPT_LIB_INPUTSIMULATED
+* @brief      Script Lib Input Simulated class
+* @ingroup    SCRIPT
+* 
+* @copyright  GEN Group. All rights reserved.
+* 
+* @cond
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+* documentation files(the "Software"), to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense, and/ or sell copies of the Software,
+* and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+* the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+* THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+* @endcond
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+
+/*---- PRECOMPILATION INCLUDES ----------------------------------------------------------------------------------------*/
+#pragma region PRECOMPILATION_INCLUDES
+
+#include "GEN_Defines.h"
+
+#include "Script_Lib_InputSimulated.h"
+
+#pragma endregion
+
+
+/*---- INCLUDES ------------------------------------------------------------------------------------------------------*/
+#pragma region INCLUDES
+
+#include "XVariant.h"
+
+#include "APPBase.h"
+#include "APPMain.h"
+
+#include "INPFactory.h"
+#include "INPSimulated.h"
+
+#include "Script.h"
+
+#include "XMemory_Control.h"
+
+
+#pragma endregion
+
+
+/*---- GENERAL VARIABLE ----------------------------------------------------------------------------------------------*/
+#pragma region GENERAL_VARIABLE
+
+
+#pragma endregion
+
+
+/*---- CLASS MEMBERS -------------------------------------------------------------------------------------------------*/
+#pragma region CLASS_MEMBERS
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn        SCRIPT_LIB_INPUTSIMULATED::SCRIPT_LIB_INPUTSIMULATED()
+* @brief      Constructor
+* @ingroup    SCRIPT
+* 
+* @return     Does not return anything. 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+SCRIPT_LIB_INPUTSIMULATED::SCRIPT_LIB_INPUTSIMULATED() : SCRIPT_LIB(SCRIPT_LIB_NAME_PROCESS)
+{
+  Clean();
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn        SCRIPT_LIB_INPUTSIMULATED::~SCRIPT_LIB_INPUTSIMULATED()
+* @brief      Destructor
+* @note       VIRTUAL
+* @ingroup    SCRIPT
+* 
+* @return     Does not return anything. 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+SCRIPT_LIB_INPUTSIMULATED::~SCRIPT_LIB_INPUTSIMULATED()
+{
+  Clean();
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         boolSCRIPT_LIB_INPUTSIMULATED::AddLibraryFunctions(SCRIPT* script)
+* @brief      AddLibraryFunctions
+* @ingroup    SCRIPT
+* 
+* @param[in]  script : 
+* 
+* @return     bool : true if is succesful. 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+bool SCRIPT_LIB_INPUTSIMULATED::AddLibraryFunctions(SCRIPT* script)
+{
+  if(!script) return false;
+
+  this->script = script;
+
+  script->AddLibraryFunction(this, __L("PressKey")                  , Call_PressKey);
+  
+  return true;
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         voidSCRIPT_LIB_INPUTSIMULATED::Clean()
+* @brief      Clean the attributes of the class: Default initialice
+* @note       INTERNAL
+* @ingroup    SCRIPT
+* 
+* @return     void : does not return anything. 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+void SCRIPT_LIB_INPUTSIMULATED::Clean()
+{
+
+}
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Library Functions                                                                                                  */
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         void Call_OpenURL(SCRIPT_LIB* library, SCRIPT* script, XVECTOR<XVARIANT*>* params, XVARIANT* returnvalue)
+* @brief      all_OpenURL
+* @ingroup    SCRIPT
+* 
+* @param[in]  library : 
+* @param[in]  script : 
+* @param[in]  params : 
+* @param[in]  returnvalue : 
+* 
+* @return     void : does not return anything. 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+void Call_PressKey(SCRIPT_LIB* library, SCRIPT* script, XVECTOR<XVARIANT*>* params, XVARIANT* returnvalue)
+{
+  if(!library)      return;
+  if(!script)       return;
+  if(!params)       return;
+  if(!returnvalue)  return;
+
+  returnvalue->Set();
+
+  bool status  = false;
+
+  if(!params->GetSize())
+    {
+      script->HaveError(SCRIPT_ERRORCODE_INSUF_PARAMS);
+      return;
+    }
+
+  XVARIANT* variant = params->Get(0);
+  double key = (*variant);
+  
+  INPSIMULATED* inpsimulated = GEN_INPFACTORY.CreateSimulator();
+  if(!inpsimulated)
+    {
+      (*returnvalue) = status;
+      return;
+    }
+  
+  status = inpsimulated->PressKey((XBYTE)key);
+
+  (*returnvalue) = status;
+}
+
+
+#pragma endregion
+
+
+
