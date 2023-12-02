2f2a9b466b972b6b263fed2133478af4a69e5a1b
Sir Graham <sirgraham@endorasoft.com>
2023-12-02 21:50:58 +0100

    Elimina Batch compile Examples and Tests -> move to Utilities

--- Batch/erase_artifacts.bat
+++
@@ -1,6 +0,0 @@
-echo Remove directory %1
-if exist %1\Platforms\.vs (rmdir %1\Platforms\.vs /s /q)
-if exist %1\Platforms\Windows (rmdir %1\Platforms\Windows /s /q)
-if exist %1\Platforms\out (rmdir %1\Platforms\out /s /q)
-if exist %1\Platforms\Linux (rmdir %1\Platforms\Linux /s /q)
-if exist %1\Platforms\Android (rmdir %1\Platforms\Android /s /q)
