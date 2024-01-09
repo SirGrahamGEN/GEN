e155806160a877948db1840a75c5e6e234d01e17
Sir Graham <sirgraham@endorasoft.com>
2024-01-09 22:05:18 +0100

    Init Location API

---
+++ Location/LocationAddress.cpp
@@ -0,0 +1,196 @@
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @file       LocationAddress.cpp
+* 
+* @class      LOCATIONADDRESS
+* @brief      Location Address class  (container)
+* @ingroup    LOCATION
+* 
+* @copyright  GEN Group. All rights reserved.
+* 
+* @cond
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+* documentation files(the "Software"), to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense, and/ or sell copies of the Software,
+* and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+* the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+* THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+* @endcond
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+
+/*---- PRECOMPILATION INCLUDES ----------------------------------------------------------------------------------------*/
+#pragma region PRECOMPILATION_INCLUDES
+
+#include "GEN_Defines.h"
+
+#pragma endregion
+
+
+/*---- INCLUDES ------------------------------------------------------------------------------------------------------*/
+#pragma region INCLUDES
+
+#include "LocationAddress.h"
+
+#include "XMemory_Control.h"
+
+#pragma endregion
+
+
+/*---- GENERAL VARIABLE ----------------------------------------------------------------------------------------------*/
+#pragma region GENERAL_VARIABLE
+
+#pragma endregion
+
+
+/*---- CLASS MEMBERS -------------------------------------------------------------------------------------------------*/
+#pragma region CLASS_MEMBERS
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         LOCATIONADDRESS::LOCATIONADDRESS()
+* @brief      Constructor
+* @ingroup    LOCATION
+* 
+* @return     Does not return anything. 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+LOCATIONADDRESS::LOCATIONADDRESS()
+{
+  Clean();
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         LOCATIONADDRESS::~LOCATIONADDRESS()
+* @brief      Destructor
+* @note       VIRTUAL
+* @ingroup    LOCATION
+* 
+* @return     Does not return anything. 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+LOCATIONADDRESS::~LOCATIONADDRESS()
+{
+  Clean();
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         XSTRING* LOCATIONADDRESS::GetStreet()
+* @brief      GetStreet
+* @ingroup    LOCATION
+* 
+* @return     XSTRING* : 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+XSTRING* LOCATIONADDRESS::GetStreet()
+{
+  return &street;
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         XSTRING* LOCATIONADDRESS::GetCity()
+* @brief      GetCity
+* @ingroup    LOCATION
+* 
+* @return     XSTRING* : 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+XSTRING* LOCATIONADDRESS::GetCity()
+{
+  return &city;
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         XSTRING* LOCATIONADDRESS::GetState()
+* @brief      GetState
+* @ingroup    LOCATION
+* 
+* @return     XSTRING* : 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+XSTRING* LOCATIONADDRESS::GetState()
+{
+  return &state;
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         XSTRING* LOCATIONADDRESS::GetCountry()
+* @brief      GetCountry
+* @ingroup    LOCATION
+* 
+* @return     XSTRING* : 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+XSTRING* LOCATIONADDRESS::GetCountry()
+{
+  return &country;
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         XDWORD LOCATIONADDRESS::GetPostalCode()
+* @brief      GetPostalCode
+* @ingroup    LOCATION
+* 
+* @return     XDWORD : 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+XDWORD LOCATIONADDRESS::GetPostalCode()
+{
+  return postalcode;
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+* 
+* @fn         void LOCATIONADDRESS::SetPostalCode(XDWORD postalcode)
+* @brief      SetPostalCode
+* @ingroup    LOCATION
+* 
+* @param[in]  postalcode : 
+* 
+* @return     void : does not return anything. 
+* 
+* --------------------------------------------------------------------------------------------------------------------*/
+void LOCATIONADDRESS::SetPostalCode(XDWORD postalcode)
+{
+  this->postalcode = postalcode;
+}
+
+
+/**-------------------------------------------------------------------------------------------------------------------
+*
+* @fn         void LOCATIONADDRESS::Clean()
+* @brief      Clean the attributes of the class: Default initialice
+* @note       INTERNAL
+* @ingroup
+*
+* @return     void : does not return anything.
+*
+* --------------------------------------------------------------------------------------------------------------------*/
+void LOCATIONADDRESS::Clean()
+{
+  postalcode  = 0;
+}
+
+
+#pragma endregion
